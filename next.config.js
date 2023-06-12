8789b145c662b5e4af2ab912b6e60ca99ffe4bf7
liam-earle <38230039+liam-earle@users.noreply.github.com>
2023-06-12 20:48:09 -0230

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  assetPrefix: isProd ? "/" : "",
+  output: "export",
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
