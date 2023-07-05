97d2004dc4124661f8d4c6fbd1089c13b6b91edd
Russell Corbett <87535812+Foul-Trouble@users.noreply.github.com>
2023-07-05 12:43:40 -0230

    Mobile Bug Fixed

--- pages/academics/index.js
+++ pages/academics/index.js
@@ -15,7 +15,7 @@ function Navigation() {
   
     return (
       <div className="xl:flex xl:flex-col items-center">
-        <div className="flex flex-col sticky top-16 lg:top-20 pl-20 pr-20 z-50 lg:flex-row w-full justify-evenly bg-stone-400 border-b-2 border-black pt-1 lg:pt-3 pb-2 lg:pb-5 rounded-b-3xl">
+        <div className="flex flex-col sticky top-16 lg:top-20 pl-20 pr-20 z-40 lg:flex-row w-full justify-evenly bg-stone-400 border-b-2 border-black pt-1 lg:pt-3 pb-2 lg:pb-5 rounded-b-3xl">
           <button
             className={`xl:w-1/3 xl:text-3xl font-extrabold font-ops transition-transform ${
               currentButton === 1 ? 'xl:scale-125 underline text-black' : 'text-white'
