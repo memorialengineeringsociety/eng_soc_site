462b2355e4b1c093c5b13be1ceb70f0db3742049
Russell Corbett <87535812+Foul-Trouble@users.noreply.github.com>
2023-07-18 00:50:21 -0230

    Disclaimer Updated

--- components/PopUpWarning.jsx
+++ components/PopUpWarning.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const PopupWarning = () => {
+const PopupWarning = (props) => {
 	const [isOpen, setIsOpen] = useState(true);
 
 	const handleClose = () => {
@@ -10,9 +10,9 @@ const PopupWarning = () => {
 
 	return (
 		<div className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 backdrop-blur-xl transition-opacity ${isOpen ? "visible opacity-100" : "invisible opacity-0"}`} style={{ transition: "opacity 0.5s ease-out" }}>
-			<div className="flex w-3/4 flex-col rounded-2xl bg-white p-4 text-center lg:w-1/2 2xl:w-1/5">
-				<h2 className="text-2xl font-bold text-black underline">Disclaimer</h2>
-				<p className="text-center text-lg font-semibold text-black">The following includes content that is a product of its time and does not align with the values or norms of the current society. We strongly disagree with its viewpoints and encourage readers to approach this material with caution. It is crucial to recognize that societal attitudes and perspectives evolve over time, and this content serves as a reflection of past beliefs, behaviors, and cultural practices. We urge you to view this content critically, considering the progress and social change that has likely taken place since its creation.</p>
+			<div className="flex w-10/12 md:w-3/4 flex-col rounded-2xl bg-white p-4 text-center lg:w-1/2 2xl:w-2/5">
+				<h2 className="text-2xl font-bold text-black underline">{props.title}</h2>
+				<p className="text-center text-lg font-semibold text-black">{props.text}</p>
 				<button className="mt-2 rounded bg-mun-burgundy px-4 py-2 text-white" onClick={handleClose}>
 					Continue
 				</button>
