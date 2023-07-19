8268889b40c71316400adba7804f39381c8acd31
Russell Corbett <87535812+Foul-Trouble@users.noreply.github.com>
2023-07-19 11:26:01 -0230

    Navbar Fixes

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -12,10 +12,14 @@ export const Navbar = () => {
 		setActive(!active);
 	};
 
+	const handleClickClose = () => {
+		setActive(false);
+	};
+
 	return (
 		<>
 			<nav className="sticky top-0 z-50 flex flex-wrap items-center border-b-4 border-black bg-mun-burgundy p-3">
-				<Link href="/">
+				<Link href="/" onClick={handleClickClose}>
 					<div className="mr-4 inline-flex items-center p-2 ">
 						<span className="text-md font-ops font-bold tracking-wide text-white md:text-lg lg:text-xl xl:text-3xl">MUN ENGINEERING SOCIETY</span>
 					</div>
@@ -30,27 +34,27 @@ export const Navbar = () => {
 				{/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
 				<div className={`${active ? "" : "hidden"}   w-full lg:inline-flex lg:w-auto lg:flex-grow`}>
 					<div className="z-50 flex w-full flex-col items-start lg:ml-auto lg:inline-flex  lg:h-auto lg:w-auto lg:flex-row lg:items-center">
-						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClick}>
+						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClickClose}>
 							<Link href="/aboutus">
 								<div className={`w-full items-center justify-center rounded px-3 py-2 font-gold text-lg font-bold text-white hover:bg-mun-burgundy hover:text-white lg:inline-flex lg:w-auto xl:text-2xl ${router.pathname == "/" ? "text-mun-burgundy" : "text-brand-darkblue"}`}>About</div>
 							</Link>
 						</motion.div>
-						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClick}>
+						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClickClose}>
 							<Link href="/academics">
 								<div className={`w-full items-center justify-center rounded px-3 py-2 font-gold text-lg font-bold text-white hover:bg-mun-burgundy hover:text-white lg:inline-flex lg:w-auto xl:text-2xl ${router.pathname == "/" ? "text-mun-burgundy" : "text-brand-darkblue"}`}>Academics</div>
 							</Link>
 						</motion.div>
-						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClick}>
+						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClickClose}>
 							<Link href="/events">
 								<div className={`w-full items-center justify-center rounded px-3 py-2 font-gold text-lg font-bold text-white hover:bg-mun-burgundy hover:text-white lg:inline-flex lg:w-auto xl:text-2xl ${router.pathname == "/" ? "text-mun-burgundy" : "text-brand-darkblue"}`}>Events</div>
 							</Link>
 						</motion.div>
-						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClick}>
+						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClickClose}>
 							<Link href="/news">
 								<div className={`w-full items-center justify-center rounded px-3 py-2 font-gold text-lg font-bold text-white hover:bg-mun-burgundy hover:text-white lg:inline-flex lg:w-auto xl:text-2xl ${router.pathname == "/" ? "text-mun-burgundy" : "text-brand-darkblue"}`}>News</div>
 							</Link>
 						</motion.div>
-						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClick}>
+						<motion.div className="py-2" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={handleClickClose}>
 							<Link href="/getinvolved">
 								<div className={`w-full items-center justify-center rounded px-3 py-2 font-gold text-lg font-bold text-white hover:bg-mun-burgundy hover:text-white lg:inline-flex lg:w-auto xl:text-2xl ${router.pathname == "/" ? "text-mun-burgundy" : "text-brand-darkblue"}`}>Get Involved</div>
 							</Link>
