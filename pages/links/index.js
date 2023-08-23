16fc7f4a6b5c7e84936a74cd527070fcb63f2e4a
liam-earle <38230039+liam-earle@users.noreply.github.com>
2023-08-23 16:23:25 -0230

    Revert "links pulled from db"
    This reverts commit 300f376152b0c33f22dd7ecc63acce88bca9073f.

--- pages/links/index.js
+++ pages/links/index.js
@@ -1,20 +1,7 @@
 import Link from "next/link";
 import { LinkCard } from "../../components/LinkCard";
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { useEffect, useState } from 'react';
 
 export default function Events() {
-	const [links, setLinks] = useState([]);
-	const supabase = createClientComponentClient();
-
-	useEffect(() => {
-		const getData = async () => {
-			const {data} = await supabase.from('links').select();
-			setLinks(data);
-		}
-		getData();
-	}, []);
-
 	return (
 		<main className="bg-gradient-to-b from-blue-400 to-blue-950">
 			<section className="flex flex-col justify-center pt-5">
@@ -23,7 +10,13 @@ export default function Events() {
 				<h1 className="text-center font-gold text-lg text-white lg:text-2xl">MUN Engineering Undergraduate Society</h1>
 			</section>
 			<section className="flex min-h-screen flex-col place-items-center gap-y-3 p-3">
-				{links.map((link) => (<LinkCard title={link.title} link={link.link} image={link.photo_url} />))}
+				<LinkCard title="D-Day Song Requests" description="" link="https://docs.google.com/forms/d/1CJ30Uh4W7eXCFGOOcIfaZ15-W4D5c4d80H2x-p9txZY/edit" image="" />
+				<LinkCard title="Society B Junior VP External By-Election" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSeA29VG7-13R_Uo4Jo-3TZgb4jQHFrEl8Ai4onOGEG6533cCQ/viewform?vc=0&c=0&w=1&flr=0" image="" />
+				<LinkCard title="ESPN Mentee Sign Up" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSca-oEKiC05jHVY-DvjPXBE5RR5pEfD6bYGEsjnpnyI_Ayd0Q/viewform?usp=sf_link" image="" />
+				<LinkCard title="EPSN Mentor Sign Up" description="" link="https://docs.google.com/forms/d/e/1FAIpQLScO4zJypp7VDY-5swV1oJGByWpmfXdG1n8VBSByMheQNYApOw/viewform?usp=sf_link" image="" />
+				
+				<LinkCard title="Faculty Website" description="" link="https://www.mun.ca/engineering/" image="" />
+				<LinkCard title="Counselling Services" description="" link="https://www.mun.ca/studentwellness/supports-services/index.php" image="" />
 			</section>
 		</main>
 	);
