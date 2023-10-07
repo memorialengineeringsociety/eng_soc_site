8beca90d888a5c5e6cbebdd983d127f3e2db0eda
Russell Corbett <87535812+Foul-Trouble@users.noreply.github.com>
2023-10-07 13:47:04 -0230

    Update index.js

--- pages/links/index.js
+++ pages/links/index.js
@@ -11,14 +11,13 @@ export default function Events() {
 			</section>
 			<section className="flex min-h-screen flex-col place-items-center gap-y-3 p-3">
 				<LinkCard title="Eastern Edge Bus Crawl" description="" link="https://forms.gle/uNg4t1UM9F6YeAKv9" image="" />
-				<LinkCard title="Hockey Game Sign Up" description="" link="https://docs.google.com/forms/d/1s6OrfhNwr4637SwW-pw5aQ2AtFxjX96UHEWUnqgKkF8/viewform?edit_requested=true" />
-				<LinkCard title="Class Representative Voting Ballot" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSfuseF1RpOCPJyLI6uY3HjkARZ9sqgWVh6Mq59FEkzeaJ2N3w/closedform" />
-				<LinkCard title="Industry Lecture Series Sign Up" description="" link="https://docs.google.com/forms/u/1/d/e/1FAIpQLSfKFpfaoMI6L7rPwjDgcmxu_UpIN0QR2hPKrPVxeuNvcw4L0Q/viewform?usp=sf_link" image="" />
+				{/* <LinkCard title="Class Representative Voting Ballot" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSfuseF1RpOCPJyLI6uY3HjkARZ9sqgWVh6Mq59FEkzeaJ2N3w/closedform" /> */}
+				{/* <LinkCard title="Industry Lecture Series Sign Up" description="" link="https://docs.google.com/forms/u/1/d/e/1FAIpQLSfKFpfaoMI6L7rPwjDgcmxu_UpIN0QR2hPKrPVxeuNvcw4L0Q/viewform?usp=sf_link" image="" /> */}
 				<LinkCard title="Class of 2028 Facebook Group" description="" link="https://m.facebook.com/groups/763757462139761/?ref=share&mibextid=S66gvF" image="" />
-				<LinkCard title="Application for WISE US Executive Position" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSevPlBwdtJwO8vismP_ttfnERGprZ2ru4VY0YxaU85Q09gxOg/viewform?usp=sf_link" />
-				<LinkCard title="ESPN Welcome Event Sign Up" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSdl47MNubyA0FMuD6x11rkWGzgCdcqQZqExF7AwwvhtduRitw/viewform?usp=sf_link" />
-				<LinkCard title="Softball Sign Up Form" description="" link="https://docs.google.com/forms/u/5/d/e/1FAIpQLScpto8Ttzg32mvpt5Ifdzijy6L-16A0TdBYxULSJZiQ-4e_9w/viewform?usp=sf_link" />
-				<LinkCard title="Society B Junior VP External By-Election" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSeA29VG7-13R_Uo4Jo-3TZgb4jQHFrEl8Ai4onOGEG6533cCQ/viewform?vc=0&c=0&w=1&flr=0" image="" />
+				{/* <LinkCard title="Application for WISE US Executive Position" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSevPlBwdtJwO8vismP_ttfnERGprZ2ru4VY0YxaU85Q09gxOg/viewform?usp=sf_link" /> */}
+				{/* <LinkCard title="ESPN Welcome Event Sign Up" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSdl47MNubyA0FMuD6x11rkWGzgCdcqQZqExF7AwwvhtduRitw/viewform?usp=sf_link" /> */}
+				{/* <LinkCard title="Softball Sign Up Form" description="" link="https://docs.google.com/forms/u/5/d/e/1FAIpQLScpto8Ttzg32mvpt5Ifdzijy6L-16A0TdBYxULSJZiQ-4e_9w/viewform?usp=sf_link" /> */}
+				{/* <LinkCard title="Society B Junior VP External By-Election" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSeA29VG7-13R_Uo4Jo-3TZgb4jQHFrEl8Ai4onOGEG6533cCQ/viewform?vc=0&c=0&w=1&flr=0" image="" /> */}
 				<LinkCard title="ESPN Mentee Sign Up" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSca-oEKiC05jHVY-DvjPXBE5RR5pEfD6bYGEsjnpnyI_Ayd0Q/viewform?usp=sf_link" image="" />
 				<LinkCard title="EPSN Mentor Sign Up" description="" link="https://docs.google.com/forms/d/e/1FAIpQLScO4zJypp7VDY-5swV1oJGByWpmfXdG1n8VBSByMheQNYApOw/viewform?usp=sf_link" image="" />
 				<LinkCard title="Faculty Website" description="" link="https://www.mun.ca/engineering/" image="" />
