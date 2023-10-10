c69ab2ba9a602d821afef43f3826bdae3e3e6bc1
Russell Corbett <87535812+Foul-Trouble@users.noreply.github.com>
2023-10-10 12:14:33 -0230

    Links turned into JSON

--- pages/links/index.js
+++ pages/links/index.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import { LinkCard } from "../../components/LinkCard";
+import jsonData from "public/json/links.json";
+
+const data = jsonData;
 
 export default function Events() {
 	return (
@@ -10,12 +13,9 @@ export default function Events() {
 				<h1 className="text-center font-gold text-lg text-white lg:text-2xl">MUN Engineering Undergraduate Society</h1>
 			</section>
 			<section className="flex min-h-screen flex-col place-items-center gap-y-3 p-3">
-				<LinkCard title="Eastern Edge Bus Crawl" description="" link="https://forms.gle/uNg4t1UM9F6YeAKv9" image="" />
-				<LinkCard title="Patagonia Order Form" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSdphowbJgaiVkwCfUslh0w9xXpM9G20VIgZ2t-H_boepsZYQA/viewform?usp=sf_link" image="" />
-				<LinkCard title="Class of 2028 Facebook Group" description="" link="https://m.facebook.com/groups/763757462139761/?ref=share&mibextid=S66gvF" image="" />
-				<LinkCard title="Faculty Website" description="" link="https://www.mun.ca/engineering/" image="" />
-				<LinkCard title="Counselling Services" description="" link="https://www.mun.ca/studentwellness/supports-services/index.php" image="" />
-				<LinkCard title="Professor's Appreciation Award" description="" link="https://docs.google.com/forms/d/e/1FAIpQLSfHdqHc-iI1v6IDTkIUjPIdB4qFZ5tfrVSFNUHKMiYj1rKKYA/viewform?usp=sharing" image="" />
+				{Object.entries(data).map(([event, event_data]) => {
+					return <LinkCard title={event} description={event_data.description} link={event_data.link} image={event_data.image}/>;
+				})}
 			</section>
 		</main>
 	);
